7484a034810624eb3d20e53a030ca32ed9607608
Keenan Payne <contact@keenanpayne.com>
2017-08-12 16:39:41 -0700

    Add Disqus commenting, work on lazy loading

--- site.js
+++ site.js
@@ -92,6 +92,67 @@
           '.post'
         ], '-animated'
       );
+    },
+
+
+    /**
+     * findTop()
+     */
+    findTop: function(obj) {
+      var curtop = 0;
+
+      if (obj.offsetParent) {
+        do {
+          curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+
+        return curtop;
+      }
+    },
+
+
+    /**
+     * disqusEmbed()
+     *
+     * Load Disqus comments per embed code
+     *
+     * @returns null
+     */
+    disqusEmbed: function() {
+      var disqus_shortname = 'keenan-payne';
+      var dsq = document.createElement('script');
+      dsq.type = 'text/javascript';
+      dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+
+      disqusLoaded = true;
+    },
+
+
+    /**
+     * loadDisqus()
+     *
+     * @returns null
+     */
+    loadDisqus() {
+      var comments = document.getElementsByClassName('comments')[0];
+      var disqusLoaded = false;
+
+      if(window.location.hash.indexOf('#comments') > 0) {
+        Helpers.disqusEmbed();
+      }
+
+      if(comments) {
+        var commentsOffset = Helpers.findTop(comments);
+
+        window.onscroll = function() {
+          if(!disqusLoaded && window.pageYOffset > commentsOffset - 1000) {
+            Helpers.disqusEmbed();
+          }
+        }
+      }
     }
   };
 
@@ -101,6 +162,14 @@
    */
   $(document).ready(function() {
     Helpers.siteAnimations();
+
+    var disqus_shortname = 'keenan-payne';
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    disqusLoaded = true;
   });
 
   $(window).on('scroll resize', function() {
