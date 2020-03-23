f0c16acf89e758f67256676cc6b9703f10d7b3f0
Keenan Payne <contact@keenanpayne.com>
2020-03-22 19:37:36 -0700

    First pass at implementing Eleventy + build tools

---
+++ new/.eleventy.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dir: {
+    input: "views",
+    output: "_site",
+    includes: "_templates",
+    templateFormats: ["html", "njk"]
+  }
+};
---
+++ new/views/index.md
@@ -0,0 +1,4 @@
+---
+layout: ac
+title: "Home"
+---
