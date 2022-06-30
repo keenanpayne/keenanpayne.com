66e6f00d7e0abf3433e2c7c793a019cd0f5ece19
Keenan Payne <contact@keenanpayne.com>
2022-06-29 17:41:57 -0700

    Emphasize phrases

--- _data/portfolio/gofundme.js
+++ _data/portfolio/gofundme.js
@@ -7,7 +7,7 @@ module.exports = {
       url: "https://www.linkedin.com/in/kelsealittle/",
       quotes: [
         "Keenan came well recommended, and after speaking with him I was impressed by his positive attitude and willingness to work into our team workflow.",
-        "We were able to launch the GoFundMe Heroes Hub, a beautiful new home for all of our editorial brand content. It has been very well received and our team <strong>loves using it</strong> to post new content.",
+        "We were able to launch the GoFundMe Heroes Hub, a beautiful new home for all of our editorial brand content. It has been very well received and <strong>our team loves using it</strong> to post new content.",
         "Keenan was amazing at communication, meeting/exceeding deadlines, and making himself available for fixes & troubleshooting in the immediate stretch after the project had launched.",
         "I would recommend Keenan very highly, he was amazing to work with and I hope to be able to work with him again in the future!"
       ],
@@ -18,7 +18,7 @@ module.exports = {
       url: "https://tonymingo.com/",
       quotes: [
         "Keenan makes a great development partner. As a designer, I appreciate his magical ability to talk about projects in common terms and teach us a few things along the way.",
-        "I would recommend Keenan to anyone. Aside from his obvious mastery of web development, he's patient, sharp, and empathetic — a rare breed in my experience.",
+        "I would recommend Keenan to anyone. Aside from his obvious mastery of web development, <strong>he's patient, sharp, and empathetic</strong> — a rare breed in my experience.",
         "Keenan Payne is like a well-designed website. Technical and complex on the inside, with an intuitive, easy-to-use UI on the outside."
       ]
     }
