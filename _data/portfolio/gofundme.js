9068e4d4c449b076fd1c61a56196452d37f2dd0d
Keenan Payne <contact@keenanpayne.com>
2022-07-16 11:41:38 -0700

    Migrate all images to Cloudinary

--- _data/portfolio/gofundme.js
+++ _data/portfolio/gofundme.js
@@ -19,7 +19,7 @@ module.exports = {
   ],
   lede: "Building a content hub to highlight people who are making a difference.",
   description: "Who hasn't heard of GoFundMe? They're the <em>most popular</em> personal fundraising website in the world and have helped facilitate <strong>$9 billion</strong> in fundraising for personal causes.",
-  cover: '/images/portfolio/gfm/dribbble.png',
+  cover: 'https://res.cloudinary.com/keenan-payne/image/upload/v1657996527/portfolio/gofundme/cover_cwiwux.png',
   pillars: {
     client: 'The marketing team at GoFundMe wanted to highlight standout projects from "heroes" on the platform.',
     challenge: "Working within an existing WordPress codebase, I had to extend post taxonomies to support the editorial format for heroes content while building a new user interface.",
@@ -51,25 +51,25 @@ module.exports = {
   dribbble: {
     [1]: {
       link: "https://dribbble.com/shots/11292376-GoFundMe-Heroes-Featured-article-carousel",
-      video: "https://cdn.dribbble.com/users/133571/screenshots/11292376/media/331109413b0560fa3ed2c40f6c83be5f.mp4",
+      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1657996399/portfolio/gofundme/carousel_yhtnmh.mp4",
       title: "Featured article carousel",
       description: "The interactive carousel that highlights featured campaigns on the GoFundMe Heroes Hub homepage.",
     },
     [2]: {
       link: "https://dribbble.com/shots/11115393-GoFundMe-Heroes-Article-template",
-      image: "https://cdn.dribbble.com/users/133571/screenshots/11115393/media/07eed17452f1dfcd19730c56fed69342.png?compress=1&resize=1200x900&vertical=top",
+      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1657996710/portfolio/gofundme/article_la1hge.png",
       title: "Article template",
       description: "",
     },
     [3]: {
       link: "https://dribbble.com/shots/11115406-GoFundMe-Heroes-Full-article-template",
-      video: "https://cdn.dribbble.com/users/133571/screenshots/11115406/media/3c4d4eab4d97a0f1fc62dd780d8cf37a.mp4",
+      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1657996399/portfolio/gofundme/article-scroll-purple_b1nuzy.mp4",
       title: "Full article template",
       description: "",
     },
     [4]: {
       link: "https://dribbble.com/shots/11115360-GoFundMe-Heroes-Landing-page",
-      video: "https://cdn.dribbble.com/users/133571/screenshots/11115360/media/5c18910360eb6df31476335f6a7f54d3.mp4",
+      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1657996399/portfolio/gofundme/homepage-scroll_y9telw.mp4",
       title: "Content hub landing page",
       description: "Highlighting the homepage for GoFundMe Heroes content",
     }
--- portfolio/gofundme.md
+++ portfolio/gofundme.md
@@ -3,7 +3,7 @@ permalink: "/portfolio/gofundme/"
 title: "GoFundMe"
 meta: 
   description: "Building a content hub to highlight people who are making a difference."
-  image: '/images/portfolio/gfm/og.png'
+  image: 'https://res.cloudinary.com/keenan-payne/image/upload/v1657996840/portfolio/gofundme/og_sqqaf6.png'
 data: gofundme
 
 # rootStyles: '--root-background: linear-gradient(180deg, rgba(25,175,110,1) 0%, rgba(150,215,180,0.5575980392156863) 30vh, rgba(239,239,239,1) 60vh, rgba(199,196,215,1) 85%);'
