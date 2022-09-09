753bf22f68c0bbb21a7c3efa482aafa6ad86d031
Keenan Payne <contact@keenanpayne.com>
2022-09-08 21:46:56 -0600

    Update content

--- _data/portfolio/asana.js
+++ _data/portfolio/asana.js
@@ -5,8 +5,7 @@ module.exports = {
   size: "Started at 70, grew to 700",
   industry: "Software",
   color: "#940060",
-  url: "https://asana.com",
-  role: "Frontend Developer",
+  role: "Web Developer",
   services: [
     "Web Development",
     "Hiring",
@@ -26,7 +25,7 @@ module.exports = {
   overview: 'During my time at Asana, I worked on cross-functional projects across many different technologies and stakeholders. I helped grow our team from 2 to 10 people, while scaling our technology to handle the requirements of a rapidly-growing company.',
   lede: "",
   description: "",
-  cover: 'https://res.cloudinary.com/keenan-payne/image/upload/v1661998554/portfolio/asana/cover_scey3k.png',
+  cover: 'https://res.cloudinary.com/keenan-payne/image/upload/v1661998554/portfolio/asana/cover_scey3k.jpg',
   dribbble: {
     [1]: {
       link: "https://dribbble.com/shots/6478133-Asana-for-Marketers-Landing-page",
@@ -38,62 +37,121 @@ module.exports = {
   assets: {
     [1]: {
       link: "https://asana.com/teams/marketing",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662587359/portfolio/asana/asana-for-marketers_tmx4lt.png",
+      image: "v1662587359/portfolio/asana/asana-for-marketers_tmx4lt.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana for Marketers",
       description: ""
     },
     [2]: {
       link: "https://asana.com/product",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671936/portfolio/asana/asana-product_neys2x.png",
+      image: "v1662671936/portfolio/asana/asana-product_neys2x.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Product",
       description: ""
     },
     [3]: {
       link: "https://asana.com/product/workload",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671930/portfolio/asana/asana-workload_t0iwqk.png",
+      image: "v1662671930/portfolio/asana/asana-workload_t0iwqk.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Workload",
       description: ""
     },
     [4]: {
       link: "https://asana.com/product/timeline",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671887/portfolio/asana/asana-timeline_uyi5l9.png",
+      image: "v1662671887/portfolio/asana/asana-timeline_uyi5l9.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Timeline",
       description: ""
     },
     [5]: {
       link: "https://asana.com/apps",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671875/portfolio/asana/asana-apps_led7a7.png",
+      image: "v1662671875/portfolio/asana/asana-apps_led7a7.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Apps",
       description: ""
     },
     [6]: {
       link: "https://blog.asana.com/",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671925/portfolio/asana/asana-blog_gty4s8.png",
+      image: "v1662671925/portfolio/asana/asana-blog_gty4s8.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Blog",
       description: ""
     },
     [7]: {
       link: "https://blog.asana.com/2022/08/ceo-political-activism/",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671909/portfolio/asana/asana-blog-article_xfyil4.png",
+      image: "v1662671909/portfolio/asana/asana-blog-article_xfyil4.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Blog Article",
       description: ""
     },
     [8]: {
       link: "https://wavelength.asana.com/",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671926/portfolio/asana/asana-wavelength_bewte4.png",
+      image: "v1662671926/portfolio/asana/asana-wavelength_bewte4.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Wavelength",
       description: ""
     },
     [9]: {
       link: "https://wavelength.asana.com/issues-post/clarity/",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662671904/portfolio/asana/asana-wavelength-issue_ofjfgp.png",
+      image: "v1662671904/portfolio/asana/asana-wavelength-issue_ofjfgp.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Wavelength Issue",
       description: ""
     },
     [10]: {
       link: "https://wavelength.asana.com/types-clarity-high-performing-teams/",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,q_auto,w_1200/v1662672327/portfolio/asana/asana-wavelength-article_vmavtk.png",
+      image: "v1662672327/portfolio/asana/asana-wavelength-article_vmavtk.jpg",
+      image_transform: "f_auto,q_auto,w_600",
       title: "Asana Wavelength Article",
+      description: ""
+    },
+    [11]: {
+      link: "https://web.archive.org/web/20171119132904/https://asana.com/how-to/marketing/full",
+      image: "v1662676809/portfolio/asana/asana-for-marketers-truncated_utzd43.jpg",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "The Marketer's Playbook",
+      description: ""
+    },
+    [12]: {
+      link: "https://web.archive.org/web/20171119132904/https://asana.com/how-to/marketing/full",
+      image: "v1662676706/portfolio/asana/asana-homepage_a1gzqf.jpg",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Asana Homepage — 2019",
+      description: ""
+    },
+    [13]: {
+      link: "https://web.archive.org/web/20171122220356/https://asana.com/pricing",
+      image: "v1662676881/portfolio/asana/asana-pricing_utd8ry.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Asana Pricing",
+      description: ""
+    },
+    [14]: {
+      link: "https://web.archive.org/web/20171130230805/https://asana.com/",
+      image: "v1662677807/portfolio/asana/asana-homepage-2017_anu1ur.jpg",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Asana Homepage — 2017",
+      description: ""
+    },
+    [15]: {
+      link: "https://web.archive.org/web/20171129223746/https://asana.com/diversity-and-inclusion",
+      image: "v1662678109/portfolio/asana/asana-diversity_jdzrdt.jpg",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Asana Diversity & Inclusion",
+      description: ""
+    },
+    [16]: {
+      link: "https://web.archive.org/web/20171129223716/https://asana.com/jobs/",
+      image: "v1662677810/portfolio/asana/asana-jobs_h0qnkq.jpg",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Asana Jobs — 2017",
+      description: ""
+    },
+    [17]: {
+      link: "https://web.archive.org/web/20171129223741/https://asana.com/company",
+      image: "v1662678105/portfolio/asana/asana-company_nto47p.jpg",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "About the Company",
       description: ""
     },
   }
--- portfolio/asana.md
+++ portfolio/asana.md
@@ -13,19 +13,18 @@ sections:
     project: asana
     contained: true
     enlarge: true
-    modifier: '-three-col -contained'
+    modifier: '-three-col'
     source: assets
     headline: 'Landing pages'
     description: |
-      I collaborated with marketers to build countless landing pages announcing new product features and corning the work management market.
-
-      It was not uncommon for me to turn around complex landing pages in two weeks at Asana. I worked on <em>many</em> tight deadlines and consistently produced high-quality work.
+      I collaborated closely with the marketing team to build landing pages for positioning the company and product, as well as announce new product features.
     items: 
       - 1
       - 2
       - 3
       - 4
       - 5
+      - 11
   - type: portfolioGrid
     project: asana
     modifier: '-one-col'
@@ -36,14 +35,38 @@ sections:
     project: asana
     contained: true
     enlarge: true
-    modifier: '-two-col -contained'
+    modifier: '-three-col'
+    source: assets
+    headline: '2017 Redesign'
+    description: 
+    items: 
+      - 14
+      - 17
+      - 16
+      - 15
+      - 13
+  - type: portfolioGrid
+    project: asana
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    source: assets
+    headline: '2019 Redesign'
+    description: 
+    items: 
+      - 12
+  - type: portfolioGrid
+    project: asana
+    contained: true
+    enlarge: true
+    modifier: '-two-col'
     source: assets
     eyebrow: 2014
     headline: 'Asana Blog'
     description: |
-      I helped consolidate multiple WordPress blogs that served different purposes. Furthermore, I took ownership over WordPress, building a custom theme for the blog and custom plugins for various functionality. 
+      My first project after joining Asana involved consolidating multiple WordPress blogs into a single blog. In doing so, I took ownership over the blogs and built a custom theme and plugins to aid content marketing efforts.
       
-      As such, I was responsible for onboarding folks at Asana to WordPress by helping manage accounts, writing documentation on the platform and our systems, and troubleshooting issues from stakeholders.
+      I led onboarding sessions for new hires who would be working on the Asana blog. I also wrote extensive documentation for the platform for content editors and developers.
     items: 
       - 6
       - 7
@@ -51,23 +74,36 @@ sections:
     project: asana
     contained: true
     enlarge: true
-    modifier: '-three-col -contained'
+    modifier: '-three-col'
     source: assets
     eyebrow: 2015
     headline: 'Wavelength'
-    description: 
+    description: |
+      Wavelength was part of a content marketing strategy geared toward positioning Asana as a thought leader in work management and company culture. 
+
+      To aid in this effort, I built a custom WordPress theme with custom taxonomies to provide our marketing team with a digital "magazine" of sorts. 
     items: 
       - 8
       - 9
       - 10
 ---
 
-## During my time at Asana...
+<!-- Highlight Webby awards and nominations -->
 
+## Behind the scenes
+
+<!-- https://fontawesome.com/icons/solar-system?s=light&f=classic -->
+### Internal style guide and pattern library
 I honed my skills in frontend architecture as I helped design and develop the first attempts at a pattern library and web style guide. As a rapidly-growing company, I anticipated the need for greater consistency across our codebase and brand identity as our marketing and sales became more aggressive. 
-- I led the charge on building a "componentized" frontend, helped establish coding standards, and reduced the amount of code written to solve common problems.
 
+I led the charge on building a "componentized" frontend, helped establish coding standards, and reduced the amount of code written to solve common problems.
+
+<!-- https://fontawesome.com/icons/person-circle-plus?s=solid&f=classic -->
+### Hiring
 Asana grew from 70 to 700 employees in my five years there, with the web development team alone increasing from 2 to 10. 
-- I helped our hiring initiatives by interviewing candidates, crafting interview questions, and making key hiring decisions.
 
+I helped our hiring initiatives by interviewing candidates, crafting interview questions, and making key hiring decisions.
+
+<!-- https://fontawesome.com/icons/users-gear?s=solid&f=classic -->
+### Developer experience
 I helped incorporate robust build processes for managing assets in our codebase for an improved developer experience.
