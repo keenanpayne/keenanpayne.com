1203bcad24b5e7084f9cba37990582f78253218a
Keenan Payne <contact@keenanpayne.com>
2022-09-11 15:12:22 -0600

    Continue fleshing out Asana case study while improving styles

--- _data/portfolio/asana.js
+++ _data/portfolio/asana.js
@@ -22,16 +22,16 @@ module.exports = {
     "AWS",
     "Python",
   ],
-  overview: 'During my time at Asana, I worked on cross-functional projects across many different technologies and stakeholders. I helped grow our team from 2 to 10 people, while scaling our technology to handle the requirements of a rapidly-growing company.',
+  overview: 'At Asana, I worked on cross-functional projects across various technologies and stakeholders. I helped grow our team from 2–10 people while scaling our technology to handle the requirements of a rapidly-growing company.',
   lede: "",
-  description: "",
+  caption: "",
   cover: 'https://res.cloudinary.com/keenan-payne/image/upload/v1661998554/portfolio/asana/cover_scey3k.jpg',
   dribbble: {
     [1]: {
       link: "https://dribbble.com/shots/6478133-Asana-for-Marketers-Landing-page",
       video: "https://res.cloudinary.com/keenan-payne/video/upload/f_auto,w_1300/v1662587263/portfolio/asana/asana-for-marketers-dribbble_syb54y.mp4",
       title: "Asana for Marketers",
-      description: "",
+      caption: "",
     },
   },
   assets: {
@@ -40,119 +40,197 @@ module.exports = {
       image: "v1662587359/portfolio/asana/asana-for-marketers_tmx4lt.jpg",
       image_transform: "f_auto,q_auto,w_600",
       title: "Asana for Marketers",
-      description: ""
+      caption: ""
     },
     [2]: {
       link: "https://asana.com/product",
       image: "v1662671936/portfolio/asana/asana-product_neys2x.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Product",
-      description: ""
+      title: "Product — 2019",
+      caption: ""
     },
     [3]: {
       link: "https://asana.com/product/workload",
       image: "v1662671930/portfolio/asana/asana-workload_t0iwqk.jpg",
       image_transform: "f_auto,q_auto,w_600",
       title: "Asana Workload",
-      description: ""
+      caption: ""
     },
     [4]: {
       link: "https://asana.com/product/timeline",
       image: "v1662671887/portfolio/asana/asana-timeline_uyi5l9.jpg",
       image_transform: "f_auto,q_auto,w_600",
       title: "Asana Timeline",
-      description: ""
+      caption: ""
     },
     [5]: {
       link: "https://asana.com/apps",
       image: "v1662671875/portfolio/asana/asana-apps_led7a7.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Apps",
-      description: ""
+      title: "Apps & Integrations — 2019",
+      caption: ""
     },
     [6]: {
       link: "https://blog.asana.com/",
       image: "v1662671925/portfolio/asana/asana-blog_gty4s8.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Blog",
-      description: ""
+      title: "Blog",
+      caption: ""
     },
     [7]: {
       link: "https://blog.asana.com/2022/08/ceo-political-activism/",
       image: "v1662671909/portfolio/asana/asana-blog-article_xfyil4.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Blog Article",
-      description: ""
+      title: "Blog Article",
+      caption: ""
     },
     [8]: {
       link: "https://wavelength.asana.com/",
       image: "v1662671926/portfolio/asana/asana-wavelength_bewte4.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Wavelength",
-      description: ""
+      title: "Wavelength",
+      caption: ""
     },
     [9]: {
       link: "https://wavelength.asana.com/issues-post/clarity/",
       image: "v1662671904/portfolio/asana/asana-wavelength-issue_ofjfgp.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Wavelength Issue",
-      description: ""
+      title: "Wavelength Issue",
+      caption: ""
     },
     [10]: {
       link: "https://wavelength.asana.com/types-clarity-high-performing-teams/",
       image: "v1662672327/portfolio/asana/asana-wavelength-article_vmavtk.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Wavelength Article",
-      description: ""
+      title: "Wavelength Article",
+      caption: ""
     },
     [11]: {
       link: "https://web.archive.org/web/20171119132904/https://asana.com/how-to/marketing/full",
       image: "v1662676809/portfolio/asana/asana-for-marketers-truncated_utzd43.jpg",
       image_transform: "f_auto,q_auto,w_600",
       title: "The Marketer's Playbook",
-      description: ""
+      caption: ""
     },
     [12]: {
       link: "https://web.archive.org/web/20171119132904/https://asana.com/how-to/marketing/full",
       image: "v1662676706/portfolio/asana/asana-homepage_a1gzqf.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Homepage — 2019",
-      description: ""
+      title: "Homepage — 2019",
+      caption: ""
     },
     [13]: {
       link: "https://web.archive.org/web/20171122220356/https://asana.com/pricing",
       image: "v1662676881/portfolio/asana/asana-pricing_utd8ry.png",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Pricing",
-      description: ""
+      title: "Pricing — 2017",
+      caption: ""
     },
     [14]: {
       link: "https://web.archive.org/web/20171130230805/https://asana.com/",
       image: "v1662677807/portfolio/asana/asana-homepage-2017_anu1ur.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Homepage — 2017",
-      description: ""
+      title: "Homepage — 2017",
+      caption: ""
     },
     [15]: {
       link: "https://web.archive.org/web/20171129223746/https://asana.com/diversity-and-inclusion",
       image: "v1662678109/portfolio/asana/asana-diversity_jdzrdt.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Diversity & Inclusion",
-      description: ""
+      title: "Diversity & Inclusion",
+      caption: ""
     },
     [16]: {
       link: "https://web.archive.org/web/20171129223716/https://asana.com/jobs/",
       image: "v1662677810/portfolio/asana/asana-jobs_h0qnkq.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "Asana Jobs — 2017",
-      description: ""
+      title: "Careers — 2017",
+      caption: ""
     },
     [17]: {
       link: "https://web.archive.org/web/20171129223741/https://asana.com/company",
       image: "v1662678105/portfolio/asana/asana-company_nto47p.jpg",
       image_transform: "f_auto,q_auto,w_600",
-      title: "About the Company",
-      description: ""
+      title: "Company — 2017",
+      caption: ""
+    },
+    [18]: {
+      link: "https://web.archive.org/web/20171101202801/https://asana.com/product",
+      image: "v1662925200/portfolio/asana/asana-product-2017_umgxbx.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Product — 2017",
+      caption: ""
+    },
+    [19]: {
+      link: "https://web.archive.org/web/20171101202900/https://asana.com/apps",
+      image: "v1662925201/portfolio/asana/asana-apps-2017_nxkmir.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Apps & Integrations — 2017",
+      caption: ""
+    },
+    [20]: {
+      link: "https://web.archive.org/web/20190405051809/https://asana.com/developers",
+      image: "v1662926174/portfolio/asana/asana-developers_smlsvq.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Developers Homepage",
+      caption: ""
+    },
+    [21]: {
+      link: "https://web.archive.org/web/20190106104335/https://asana.com/developers/documentation/examples-tutorials/overview",
+      image: "v1662926175/portfolio/asana/asana-developers-article_ezv5gq.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Article",
+      caption: ""
+    },
+    [22]: {
+      link: "https://web.archive.org/web/20180717200854/https://asana.com/developers/api-reference/attachments",
+      image: "v1662926176/portfolio/asana/asana-devlopers-documentation_fdyadr.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "API Documentation",
+      caption: ""
+    },
+    [23]: {
+      link: "https://web.archive.org/web/20180717200854/https://asana.com/developers/api-reference/attachments",
+      image: "v1662926173/portfolio/asana/asana-developers-api-explorer_xriekl.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Asana API Explorer",
+      caption: 'Built by <a href="https://www.linkedin.com/in/epelz/">Eric Pelz</a>'
+    },
+    [24]: {
+      link: "https://web.archive.org/web/20190413124418/https://asana.com/pricing",
+      image: "v1662927995/portfolio/asana/asana-2019-pricing_yv0xf9.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Pricing — 2019",
+    },
+    [25]: {
+      link: "https://web.archive.org/web/20190329043747/https://asana.com/jobs",
+      image: "v1662928580/portfolio/asana/asana-careers-2019_hgoysp.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Careers — 2019",
+    },
+    [26]: {
+      link: "https://web.archive.org/web/20171122220424/https://asana.com/press",
+      image: "v1662929205/portfolio/asana/asana-press-2017_dnb2nq.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Press — 2017",
+    },
+    [27]: {
+      link: "https://web.archive.org/web/20190630214525/https://asana.com/#productTour",
+      image: "v1662929522/portfolio/asana/asana-product-tour-personas_ixflhb.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Use-case selection",
+    },
+    [28]: {
+      link: "https://web.archive.org/web/20190630214525/https://asana.com/#productTour?lesson=marketing-campaign-3",
+      image: "v1662929519/portfolio/asana/asana-product-tour-lesson_nruk7b.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Use-case lesson",
+    },
+    [29]: {
+      link: "https://web.archive.org/web/20190630214525/https://asana.com/#productTour?lesson=marketing-campaign-9",
+      image: "v1662929518/portfolio/asana/asana-product-tour-finished_eas3ua.png",
+      image_transform: "f_auto,q_auto,w_600",
+      title: "Use-case finished",
     },
   }
 }
--- portfolio/asana.md
+++ portfolio/asana.md
@@ -15,46 +15,32 @@ sections:
     enlarge: true
     modifier: '-three-col'
     source: assets
-    headline: 'Landing pages'
-    description: |
-      I collaborated closely with the marketing team to build landing pages for positioning the company and product, as well as announce new product features.
-    items: 
-      - 1
-      - 2
-      - 3
-      - 4
-      - 5
-      - 11
-  - type: portfolioGrid
-    project: asana
-    modifier: '-one-col'
-    source: dribbble
-    items: 
-      - 1
-  - type: portfolioGrid
-    project: asana
-    contained: true
-    enlarge: true
-    modifier: '-three-col'
-    source: assets
-    headline: '2017 Redesign'
-    description: 
+    eyebrow: 2017
+    headline: 'Asana.com Redesign'
+    description: I helped spearhead a redesign and technical overhaul of Asana.com to support the ongoing evolution of the product and brand.
     items: 
       - 14
+      - 18
       - 17
       - 16
-      - 15
       - 13
+      - 19
+      - 26
   - type: portfolioGrid
     project: asana
     contained: true
     enlarge: true
     modifier: '-three-col'
     source: assets
-    headline: '2019 Redesign'
-    description: 
+    eyebrow: 2019
+    headline: 'Visual Overhaul'
+    description: I redesigned our most highly-trafficked pages while providing a visual facelift to styles shared across our web properties (e.g., typography, spacing, fonts, colors).
     items: 
       - 12
+      - 2
+      - 25
+      - 24
+      - 5
   - type: portfolioGrid
     project: asana
     contained: true
@@ -64,9 +50,7 @@ sections:
     eyebrow: 2014
     headline: 'Asana Blog'
     description: |
-      My first project after joining Asana involved consolidating multiple WordPress blogs into a single blog. In doing so, I took ownership over the blogs and built a custom theme and plugins to aid content marketing efforts.
-      
-      I led onboarding sessions for new hires who would be working on the Asana blog. I also wrote extensive documentation for the platform for content editors and developers.
+      I was the technical owner of Asana's WordPress blogs. I developed custom themes and plugins, wrote documentation for content editors and developers, and led onboarding sessions for new hires who would be working with the platform.
     items: 
       - 6
       - 7
@@ -76,19 +60,74 @@ sections:
     enlarge: true
     modifier: '-three-col'
     source: assets
+    headline: 'Landing pages'
+    description: |
+      I collaborated closely with the marketing and brand design teams to build landing pages for positioning and marketing Asana.
+    items: 
+      - 1
+      - 4
+      - 3
+      - 11
+  # - type: portfolioGrid
+  #   project: asana
+  #   modifier: '-one-col'
+  #   source: dribbble
+  #   contained: true
+  #   items: 
+  #     - 1
+  - type: portfolioGrid
+    project: asana
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    source: assets
     eyebrow: 2015
+    headline: 'Asana for Developers'
+    description: I built the initial documentation platform for Asana, which included API documentation and a news portal.
+    items: 
+      - 20
+      - 21
+      - 22
+      - 23
+  - type: portfolioGrid
+    project: asana
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    source: assets
+    eyebrow: 2017
     headline: 'Wavelength'
     description: |
-      Wavelength was part of a content marketing strategy geared toward positioning Asana as a thought leader in work management and company culture. 
+      Wavelength was a content marketing strategy to position Asana as a thought leader in work management and corporate leadership.
 
-      To aid in this effort, I built a custom WordPress theme with custom taxonomies to provide our marketing team with a digital "magazine" of sorts. 
+      I built a custom WordPress theme for the Wavelength website, along with custom taxonomies to portray it as a digital magazine.
     items: 
       - 8
       - 9
       - 10
+  - type: portfolioGrid
+    project: asana
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    source: assets
+    eyebrow: 2018
+    headline: 'Product Tour'
+    description: |
+      I built a "Product Tour" interactive component to aid our marketing efforts for specific product use cases.
+
+      We relied heavily on imagery and animations throughout the component, causing me to consider how to mitigate performance implications carefully.
+
+      I implemented deep linking for each page in this component, which allowed our marketing team to track and reference specific pieces of content.
+    items: 
+      - 27
+      - 28
+      - 29
 ---
 
 <!-- Highlight Webby awards and nominations -->
+
+I'm quite proud of how many things I built so long ago are still in use today. Hell, even the Asana blog is standing strong atop my 2014 code!
 
 ## Behind the scenes
 
