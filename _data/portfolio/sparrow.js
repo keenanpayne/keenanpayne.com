98cd91bbcb8a799aa56f6984f77191cdca488449
Keenan Payne <contact@keenanpayne.com>
2022-10-11 18:42:12 -0700

    Begin scaffolding Sparrow case study

---
+++ _data/portfolio/sparrow.js
@@ -0,0 +1,47 @@
+module.exports = {
+  name: "Sparrow",
+  project: "Website Rebuild",
+  year: "2021",
+  size: "50–100",
+  industry: "Software",
+  color: "#3ED0DA",
+  role: "Web Developer",
+  services: [
+    "Web Development",
+  ],
+  technologies: [
+    "HTML",
+    "CSS",
+    "Sass",
+    "JavaScript",
+    "Contentful",
+    "Eleventy",
+  ],
+  overview: "",
+  lede: "",
+  caption: "",
+  people: {
+    [1]: {
+      name: "Keenan Payne"
+    },
+    [2]: {
+      name: "All of play studio"
+    }
+  },
+  testimonials: {
+    [1]: {
+      person_id: 1,
+      content: ""
+    }
+  },
+  cover: '',
+  assets: {
+    [1]: {
+      link: "https://asana.com/teams/marketing",
+      image: "v1662587359/portfolio/asana/asana-for-marketers_tmx4lt.jpg",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Asana for Marketers",
+      caption: ""
+    },
+  },
+}
---
+++ portfolio/sparrow.md
@@ -0,0 +1,27 @@
+---
+permalink: '/portfolio/sparrow/'
+title: 'Sparrrow'
+meta: 
+  description: 
+  image: ''
+data: sparrow
+
+cover: ''
+
+sections: 
+  - type: portfolioGrid
+    project: sparrow
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 1
+  - type: testimonial
+    project: sparrow
+    person: 1
+    testimonial: 1
+    hide_citation: true
+---
