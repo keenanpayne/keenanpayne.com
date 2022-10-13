facd613b6fafe49f61fed856295888bd97ff813a
Keenan Payne <contact@keenanpayne.com>
2022-10-12 21:45:40 -0700

    Begin scaffolding Transform case study

---
+++ _data/portfolio/transform.js
@@ -0,0 +1,44 @@
+module.exports = {
+  name: "Transform",
+  project: "Website Rebuild",
+  year: "2021",
+  size: "11–50",
+  industry: "Data Software",
+  color: "#483FE1",
+  role: "Web Developer",
+  url: "https://transform.co",
+  services: [
+    "Web Development",
+  ],
+  technologies: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Eleventy",
+    "Netlify CMS",
+  ],
+  overview: "Transform is the first centralized ‘metrics store’ that empowers data analysts to deliver accurate, timely, confident, and fast insights.",
+  lede: "",
+  cover: "",
+  caption: "",
+  people: {
+    [1]: {
+      name: "Everyone from Play Studio"
+    }
+  },
+  testimonials: {
+    [1]: {
+      person_id: 1,
+      content: ""
+    }
+  },
+  assets: {
+    link: "",
+    shot_link: "",
+    image: "",
+    video: "",
+    asset_transform: "f_auto,q_auto",
+    title: "",
+    caption: ""
+  }
+}
---
+++ portfolio/transform.md
@@ -0,0 +1,24 @@
+---
+permalink: '/portfolio/transform/'
+title: 'Transform'
+meta: 
+  description: 
+  image: ""
+data: transform
+sections: 
+  - type: portfolioGrid
+    project: transform
+    contained: true
+    enlarge: true
+    modifier: '-two-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 1
+  - type: testimonial
+    project: transform
+    person: 1
+    testimonial: 1
+    hide_citation: true
+---
