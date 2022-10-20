748f5a649e52ff14af9749f45a40e30954b469f9
Keenan Payne <contact@keenanpayne.com>
2022-10-20 09:37:17 -0700

    Begin abstracting new design into reusable components
    Here’s where the magic begins 🔮

---
+++ _data/services/frontend-development.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: "Frontend development",
+  description: "I can help with a variety of frontend development projects. From deploying scalable marketing websites to complex web applications, I've got you covered."
+}
---
+++ _data/services/hiring.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: "Hiring",
+  description: "Trying to grow your web development team? I can help craft interview processes, requirements, and find a great match for your company."
+}
