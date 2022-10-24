783e1f6318c12c64a44d549d6083d14818c0ada5
Keenan Payne <contact@keenanpayne.com>
2022-10-24 09:53:27 -0700

    Begin adding assets and content

---
+++ _data/portfolio/rippling.js
@@ -0,0 +1,78 @@
+module.exports = {
+  featured: true,
+  name: "Rippling",
+  project: "Website rebuild",
+  year: "2020–2021",
+  size: "10001 to 50000",
+  industry: "HR and IT management software",
+  color: "#ffb81c",
+  role: "Web Developer",
+  url: "https://rippling.com",
+  external_case_study: "https://www.play.studio/rippling/",
+  services: [
+    "Web Development",
+  ],
+  technologies: [
+    "HTML",
+    "CSS",
+    "Sass",
+    "JavaScript",
+    "WordPress",
+    "Lottie"
+  ],
+  overview: "",
+  lede: "",
+  cover: "v1666628553/portfolio/rippling/cover_alt_nbrk5p.png",
+  assets: {
+    [1]: {
+      shot_link: "https://dribbble.com/shots/11657790-Rippling-Homepage",
+      image: "v1666627851/portfolio/rippling/homepage_zvyfxg.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Homepage",
+      caption: ""
+    },
+    [2]: {
+      link: "",
+      shot_link: "https://dribbble.com/shots/11657790-Rippling-Homepage",
+      video: "v1666627851/portfolio/rippling/homepage_svzjje.mp4",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Homepage",
+      caption: ""
+    },
+    [3]: {
+      link: "",
+      shot_link: "https://dribbble.com/shots/11658119-Rippling-Pricing",
+      image: "v1666627885/portfolio/rippling/pricing_bvuf6h.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Pricing",
+      caption: ""
+    },
+    [4]: {
+      shot_link: "https://dribbble.com/shots/11657848-Rippling-About-Page",
+      image: "v1666627852/portfolio/rippling/about_itwn0k.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "About",
+      caption: ""
+    },
+    [5]: {
+      shot_link: "https://dribbble.com/shots/11657968-Rippling-Platform",
+      video: "v1666627851/portfolio/rippling/platform_k9wtmj.mp4",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Platform",
+      caption: ""
+    },
+    [6]: {
+      shot_link: "https://dribbble.com/shots/11658280-Rippling-Mobile-Device-Management",
+      video: "v1666627851/portfolio/rippling/mobile-device-management_rteais.mp4",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Mobile Device Management",
+      caption: ""
+    },
+    [7]: {
+      image: "v1666627851/portfolio/rippling/apps-for-everything_pxzxyv.gif",
+      asset_transform: "f_auto,q_auto,w_1300",
+      title: "Apps for Everything animation",
+      caption: ""
+    },
+  }
+}
---
+++ portfolio/rippling.md
@@ -0,0 +1,43 @@
+---
+permalink: '/portfolio/rippling/'
+title: 'Rippling'
+meta: 
+  description: ''
+  image: 'https://res.cloudinary.com/keenan-payne/image/upload/v1666628151/portfolio/rippling/cover_rcfvoh.png'
+data: rippling
+sections: 
+  - type: portfolioGrid
+    project: rippling
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 1
+      - 3
+      - 4
+  - type: portfolioGrid
+    project: rippling
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 2
+      - 5
+      - 6
+  - type: portfolioGrid
+    project: rippling
+    contained: true
+    enlarge: true
+    modifier: '-one-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 7
+---
