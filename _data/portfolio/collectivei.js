3190ca8c7c5e0d2e36514588e2a2c36d7237ee3a
Keenan Payne <contact@keenanpayne.com>
2022-10-26 11:39:13 -0700

    Publish Collective[i] case study

---
+++ _data/portfolio/collectivei.js
@@ -0,0 +1,127 @@
+module.exports = {
+  name: "Collective Intelligence",
+  project: "Website rebuild",
+  year: "2021",
+  size: "51 to 100",
+  industry: "AI/ML Software",
+  color: "#384acf",
+  role: "Web Developer",
+  url: "http://collectivei.com",
+  services: [
+    "Web Development",
+  ],
+  technologies: [
+    "HTML",
+    "CSS",
+    "Sass",
+    "JavaScript",
+    "Contentful",
+    "Eleventy",
+    "Lottie",
+    "SalesForce Pardot"
+  ],
+  caption: "",
+  lede: "Building a website for a company using AI and ML to grow, manage, and predict revenue.",
+  overview: "I rebuilt the Collective[i] website from the ground up to help the company position itself as an innovative solution to revenue growth, management, and predictions using its artificial intelligence and machine learning platform.",
+  cover: "v1666806684/portfolio/collectivei/cover_tolwvc.png",
+  people: {
+    [1]: {
+      name: "Play Studio",
+      position: "Design Partner",
+      url: "https://www.play.studio/"
+    },
+    [2]: {
+      name: "Jaime Hernández",
+      position: "Development Partner",
+      url: "https://jaime.one/"
+    },
+  },
+  testimonials: {
+    [1]: {
+      person_id: 1,
+      content: ""
+    }
+  },
+  assets: {
+    [1]: {
+      link: "https://collectivei.com/",
+      image: "v1666802622/portfolio/collectivei/homepage_sdeq7c.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Homepage",
+      caption: "",
+      preventLazy: true
+    },
+    [2]: {
+      link: "https://collectivei.com/product/",
+      image: "v1666802622/portfolio/collectivei/product_qbxuzg.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Product",
+      caption: "",
+      preventLazy: true
+    },
+    [3]: {
+      link: "https://collectivei.com/careers/",
+      image: "v1666802623/portfolio/collectivei/careers_ihzhjx.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Careers",
+      caption: ""
+    },
+    [4]: {
+      link: "https://collectivei.com/news/",
+      image: "v1666802622/portfolio/collectivei/news_bf9jel.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "News",
+      caption: ""
+    },
+    [5]: {
+      link: "https://collectivei.com/news/employees-may-need-to-keep-up-the-pretense-of-working-as-automation-spreads/",
+      image: "v1666802622/portfolio/collectivei/article_mir87w.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Article",
+      caption: ""
+    },
+    [6]: {
+      link: "https://collectivei.com/trust-center/",
+      image: "v1666802622/portfolio/collectivei/security_gzqgof.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Security",
+      caption: ""
+    },
+    [7]: {
+      link: "https://collectivei.com/contact/",
+      image: "v1666802623/portfolio/collectivei/contact_yayses.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Contact",
+      caption: ""
+    },
+    [8]: {
+      link: "https://collectivei.com/#get-started",
+      image: "v1666802622/portfolio/collectivei/request-demo-cta_t7z1m7.png",
+      asset_transform: "f_auto,q_auto,w_800",
+      title: "Request a Demo",
+      caption: ""
+    },
+    [9]: {
+      link: "https://collectivei.com/#forecasting",
+      video: "v1666808142/portfolio/collectivei/forecast_zuvrwp.mp4",
+      asset_transform: "f_auto,q_auto,w_1000",
+      title: "Forecasts",
+      caption: "",
+    },
+    [10]: {
+      link: "https://collectivei.com/product/#intelligent-insightstm",
+      video: "v1666807916/portfolio/collectivei/insights_ayppcj.mp4",
+      asset_transform: "f_auto,q_auto,w_1000",
+      title: "Intelligent Insights",
+      caption: "",
+    },
+    [11]: {
+      link: "https://collectivei.com/product#virtual-dealroomstm",
+      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1666808439/portfolio/collectivei/virtual-dealrooms_sbf59j.mp4",
+      asset_transform: "f_auto,q_auto,w_1300",
+      title: "Virtual DealRooms",
+      caption: "",
+      autoplay: true
+    },
+  }
+}
---
+++ portfolio/collective-intelligence.md
@@ -0,0 +1,56 @@
+---
+permalink: '/portfolio/collective-intelligence/'
+title: 'Collective Intelligence'
+meta: 
+  description: 
+  image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666806684/portfolio/collectivei/cover_tolwvc.png"
+data: collectivei
+sections: 
+  - type: portfolioGrid
+    project: collectivei
+    contained: true
+    enlarge: true
+    modifier: '-two-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 1
+      - 2
+  - type: portfolioGrid
+    project: collectivei
+    contained: true
+    enlarge: true
+    modifier: '-three-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 3
+      - 4
+      - 5
+      - 6
+      - 7
+      - 8
+  - type: portfolioGrid
+    project: collectivei
+    contained: true
+    enlarge: true
+    modifier: '-one-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 11
+  - type: portfolioGrid
+    project: collectivei
+    contained: true
+    enlarge: true
+    modifier: '-two-col'
+    eyebrow: ''
+    headline: ''
+    description: ''
+    items: 
+      - 9
+      - 10
+---
