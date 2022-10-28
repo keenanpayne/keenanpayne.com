a18b49185c0a0706f5d7b5678749dc5bb050ea65
Keenan Payne <contact@keenanpayne.com>
2022-10-27 21:02:00 -0700

    Publish at.inc case study

---
+++ _data/portfolio/atinc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  name: "at.inc/",
+  project: "Website redesign",
+  year: "2020",
+  size: "2",
+  industry: "Venture Capital",
+  color: "#badfa4",
+  role: "Web Developer",
+  url: "https://www.at.inc/",
+  services: [
+    "Web Development",
+  ],
+  technologies: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Netlify CMS",
+    "Lottie"
+  ],
+  overview: "at.inc is a venture capital firm that invests in early-stage startups. They needed a new website to showcase their portfolio and provide a platform for their team to share their expertise.",
+  lede: "Helping an early-stage venture capital firm showcase their portfolio and services.",
+  cover: "v1666928996/portfolio/atinc/cover_hotv7m.png",
+  people: {
+    [1]: {
+      name: "Paladar Studio",
+      position: "Design Partner",
+      url: "https://www.paladarstudio.com"
+    },
+    [2]: {
+      name: "Jaime Hernández",
+      position: "Development Partner",
+      url: "https://jaime.one/"
+    },
+  },
+  assets: {
+    [1]: {
+      link: "https://www.at.inc/",
+      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666928701/portfolio/atinc/website_nqulvz.png",
+      asset_transform: "f_auto,q_auto,w_1500",
+      title: "Homepage",
+    },
+    [2]: {
+      link: "https://www.at.inc/",
+      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1666929545/portfolio/atinc/website_mc5o3p.mp4",
+      asset_transform: "f_auto,q_auto,w_1500",
+      title: "Homepage",
+    }
+  },
+}
