1827f9c873905cf46411916e3b8433b2442e75df
Keenan Payne <contact@keenanpayne.com>
2022-10-27 21:26:49 -0700

    Refactor Capital case study

---
+++ _data/portfolio/refactor-capital.js
@@ -0,0 +1,74 @@
+module.exports = {
+  name: "Refactor Capital",
+  project: "Website rebuild",
+  year: "2021",
+  size: "1",
+  industry: "Venture Capital",
+  color: "#963326",
+  role: "Web Developer",
+  url: "https://refactor.com",
+  services: [
+    "Web Development",
+  ],
+  technologies: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Netlify CMS",
+    "Lottie"
+  ],
+  overview: "Refactor Capital is an early-stage venture capital firm that invests in companies that deploy technology that saves lives and our planet.",
+  lede: "Building a website for an early-stage venture capital firm investing in companies that save lives and our planet.",
+  cover: "v1666930080/portfolio/refactorcapital/cover_azg7re.png",
+  people: {
+    [1]: {
+      name: "Play Studio",
+      position: "Design Partner",
+      url: "https://www.play.studio/refactor/"
+    },
+    [2]: {
+      name: "Jaime Hernández",
+      position: "Development Partner",
+      url: "https://jaime.one/"
+    },
+    [3]: {
+      name: "Nels Anderek",
+      position: "Development Partner",
+      url: "https://www.linkedin.com/in/nelsandereck/"
+    },
+  },
+  assets: {
+    [1]: {
+      link: "https://refactor.com/",
+      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666931115/portfolio/refactorcapital/homepage_ih7sr0.png",
+      asset_transform: "f_auto,q_auto,w_1000",
+      title: "Homepage",
+      preventLazy: true
+    },
+    [2]: {
+      link: "https://refactor.com/contact/",
+      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666931115/portfolio/refactorcapital/contact_hocufw.png",
+      asset_transform: "f_auto,q_auto,w_1000",
+      title: "Contact",
+      preventLazy: true
+    },
+    [3]: {
+      link: "https://refactor.com/portfolio/",
+      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666931115/portfolio/refactorcapital/portfolio_vqugik.png",
+      asset_transform: "f_auto,q_auto,w_1000",
+      title: "Portfolio",
+    },
+    [4]: {
+      link: "https://refactor.com/team/",
+      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666931115/portfolio/refactorcapital/team_ya0ske.png",
+      asset_transform: "f_auto,q_auto,w_1000",
+      title: "Team",
+    },
+    [5]: {
+      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1666930954/portfolio/refactorcapital/logo_jm9grh.mp4",
+      asset_transform: "f_auto,q_auto",
+      title: "Logo Animation",
+      autoplay: true
+    },
+  },
+}
---
+++ portfolio/refactor-capital.md
@@ -0,0 +1,25 @@
+---
+permalink: '/portfolio/refactor-capital/'
+title: 'Refactor Capital'
+meta: 
+  description: "Building a website for an early-stage venture capital firm investing in companies that save lives and our planet."
+  image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666930080/portfolio/refactorcapital/cover_azg7re.png"
+data: refactor-capital
+sections: 
+  - type: portfolioGrid
+    project: refactor-capital
+    contained: true
+    enlarge: true
+    modifier: '-two-col'
+    items: 
+      - 1
+      - 3
+      - 4
+      - 2
+  - type: portfolioGrid
+    project: refactor-capital
+    contained: true
+    modifier: '-one-col'
+    items: 
+      - 5
+---
