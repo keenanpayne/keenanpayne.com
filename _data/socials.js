bb33c2a4f91123fbd0479dbb17ca3e3987ac888d
Keenan Payne <contact@keenanpayne.com>
2022-10-29 18:43:44 -0700

    Add social icons to footer

---
+++ _data/socials.js
@@ -0,0 +1,22 @@
+module.exports = {
+  [1]: {
+    name: "Twitter",
+    url: "https://twitter.com/KeenanPayne_",
+  },
+  [2]: {
+    name: "LinkedIn",
+    url: "https://linkedin.com/in/keenanpayne",
+  },
+  [3]: {
+    name: "Dribbble",
+    url: "https://dribbble.com/keenanpayne",
+  },
+  [4]: {
+    name: "Behance",
+    url: "https://behance.com/keenanpayne",
+  },
+  [5]: {
+    name: "CodePen",
+    url: "https://codepen.com/keenanpayne",
+  },
+}
