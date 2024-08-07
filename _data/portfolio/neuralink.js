eee657546b371882c6ec86c7534001a566ff31ba
Keenan Payne <contact@keenanpayne.com>
2024-08-07 14:50:32 -0600

    Update Neuralink case study

--- _data/portfolio/neuralink.js
+++ _data/portfolio/neuralink.js
@@ -5,9 +5,9 @@ module.exports = {
   year: "2020–2021",
   size: "251 to 500",
   industry: "Medical technology",
-  color: "#A7CEEF",
+  color: "#b4bdcb",
   role: "Web Developer",
-  url: "https://neuralink.com",
+  url: "https://web.archive.org/web/20210101042939/https://neuralink.com/",
   services: [
     "Web development",
     "Project management"
@@ -24,67 +24,67 @@ module.exports = {
   cover: "v1667235215/portfolio/neuralink/cover_cuoloi.png",
   people: {
     [1]: {
-      name: "Play Studio",
-      position: "Design Partner",
-      url: "https://www.play.studio/neuralink/"
-    },
-    [2]: {
       name: "Jaime Hernández",
       position: "Development Partner",
       url: "https://jaime.one/"
-    }
+    },
+    [2]: {
+      name: "Play Studio",
+      position: "Design Partner",
+      url: "https://www.play.studio/"
+    },
   },
   assets: {
     [1]: {
-      link: "https://neuralink.com/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231956/portfolio/neuralink/homepage_q19ogm.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Homepage",
     },
     [2]: {
-      link: "https://neuralink.com/science/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/science/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231957/portfolio/neuralink/science_zf1qbq.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Science",
     },
     [3]: {
-      link: "https://neuralink.com/applications/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/applications/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231956/portfolio/neuralink/applications_mn37f0.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Applications",
     },
     [4]: {
-      link: "https://neuralink.com/approach/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/approach/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231956/portfolio/neuralink/technology_j4qbwu.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Approach",
     },
     [5]: {
-      link: "https://neuralink.com/about/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/about/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231957/portfolio/neuralink/about_p2ponk.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "About",
     },
     [6]: {
-      link: "https://neuralink.com/careers/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/careers/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231956/portfolio/neuralink/careers_ibn6ll.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Careers",
     },
     [7]: {
-      link: "https://neuralink.com/blog/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/blog/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231956/portfolio/neuralink/blog_eikngn.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Blog",
     },
     [8]: {
-      link: "https://neuralink.com/blog/monkey-mindpong/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/blog/monkey-mindpong/",
       image: "https://res.cloudinary.com/keenan-payne/image/upload/v1667231956/portfolio/neuralink/article_yvficu.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Blog article",
     },
     [9]: {
-      link: "https://neuralink.com/applications/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/applications/",
       video: "https://res.cloudinary.com/keenan-payne/video/upload/v1667233266/portfolio/neuralink/applications-neural-engineering_u3orn9.mp4",
       asset_transform: "f_auto,q_auto,w_1300",
       title: "",
@@ -120,14 +120,14 @@ module.exports = {
     },
     [14]: {
       shot_link: "https://dribbble.com/shots/15173370-Neuralink-Applications",
-      link: "https://neuralink.com/applications/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/applications/",
       video: "https://res.cloudinary.com/keenan-payne/video/upload/v1667233886/portfolio/neuralink/applications-scroll_znx4yl.mp4",
       asset_transform: "f_auto,q_auto,w_1000",
       title: "Applications",
     },
     [15]: {
       shot_link: "https://dribbble.com/shots/15173387-Neuralink-Approach",
-      link: "https://neuralink.com/approach/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/approach/",
       video: "https://res.cloudinary.com/keenan-payne/video/upload/v1667233887/portfolio/neuralink/approach_aoqhah.mp4",
       asset_transform: "f_auto,q_auto,w_1300",
       title: "Approach",
@@ -135,7 +135,7 @@ module.exports = {
     },
     [16]: {
       shot_link: "https://dribbble.com/shots/15173243-Neuralink-Science",
-      link: "https://neuralink.com/science/",
+      link: "https://web.archive.org/web/20210101042939/https://neuralink.com/science/",
       video: "https://res.cloudinary.com/keenan-payne/video/upload/v1667233887/portfolio/neuralink/science-scroll_uvcrcd.mp4",
       asset_transform: "f_auto,q_auto,w_1000",
       title: "Science",
