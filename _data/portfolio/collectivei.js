a6eba24b2b0321d5b03374fd1e5eec8f6cc65703
Keenan Payne <contact@keenanpayne.com>
2024-08-07 14:50:22 -0600

    Update Collective[i] case study

--- _data/portfolio/collectivei.js
+++ _data/portfolio/collectivei.js
@@ -5,7 +5,7 @@ module.exports = {
   year: "2021",
   size: "51 to 100",
   industry: "SaaS — AI/ML sales forecasting",
-  color: "#384acf",
+  color: "#050100",
   role: "Web Developer",
   url: "http://collectivei.com",
   services: [
@@ -23,18 +23,18 @@ module.exports = {
   ],
   caption: "",
   lede: "Building a website for a company using AI and ML to grow, manage, and predict revenue.",
-  overview: "I rebuilt the Collective[i] website from the ground up to help the company position itself as an innovative solution to revenue growth, management, and predictions using its artificial intelligence and machine learning platform.",
+  overview: "I helped rebuild the Collective[i] website from the ground up to help the company position itself as an innovative solution to revenue growth, management, and predictions using its artificial intelligence and machine learning platform.",
   cover: "v1666806684/portfolio/collectivei/cover_tolwvc.png",
   people: {
     [1]: {
-      name: "Play Studio",
-      position: "Design Partner",
-      url: "https://www.play.studio/"
-    },
-    [2]: {
       name: "Jaime Hernández",
       position: "Development Partner",
       url: "https://jaime.one/"
+    },
+    [2]: {
+      name: "Play Studio",
+      position: "Design Partner",
+      url: "https://www.play.studio/"
     },
   },
   testimonials: {
@@ -45,7 +45,7 @@ module.exports = {
   },
   assets: {
     [1]: {
-      link: "https://collectivei.com/",
+      link: "https://web.archive.org/web/20211027014541/https://collectivei.com/",
       image: "v1666802622/portfolio/collectivei/homepage_sdeq7c.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Homepage",
@@ -53,7 +53,7 @@ module.exports = {
       preventLazy: true
     },
     [2]: {
-      link: "https://collectivei.com/product/",
+      link: "https://web.archive.org/web/20210917061307/https://collectivei.com/product/",
       image: "v1666802622/portfolio/collectivei/product_qbxuzg.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Product",
@@ -61,63 +61,63 @@ module.exports = {
       preventLazy: true
     },
     [3]: {
-      link: "https://collectivei.com/careers/",
+      link: "https://web.archive.org/web/20210917063839/https://collectivei.com/careers/",
       image: "v1666802623/portfolio/collectivei/careers_ihzhjx.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Careers",
       caption: ""
     },
     [4]: {
-      link: "https://collectivei.com/news/",
+      link: "https://web.archive.org/web/20211027002200/https://collectivei.com/news/",
       image: "v1666802622/portfolio/collectivei/news_bf9jel.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "News",
       caption: ""
     },
     [5]: {
-      link: "https://collectivei.com/news/employees-may-need-to-keep-up-the-pretense-of-working-as-automation-spreads/",
+      link: "https://web.archive.org/web/20210917081048/https://collectivei.com/news/employees-may-need-to-keep-up-the-pretense-of-working-as-automation-spreads/",
       image: "v1666802622/portfolio/collectivei/article_mir87w.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Article",
       caption: ""
     },
     [6]: {
-      link: "https://collectivei.com/trust-center/",
+      link: "https://web.archive.org/web/20210917064113/https://collectivei.com/trust-center/",
       image: "v1666802622/portfolio/collectivei/security_gzqgof.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Security",
       caption: ""
     },
     [7]: {
-      link: "https://collectivei.com/contact/",
+      link: "https://web.archive.org/web/20210917055910/https://collectivei.com/contact/",
       image: "v1666802623/portfolio/collectivei/contact_yayses.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Contact",
       caption: ""
     },
     [8]: {
-      link: "https://collectivei.com/#get-started",
+      link: "https://web.archive.org/web/20210917055910/https://collectivei.com/#get-started",
       image: "v1666802622/portfolio/collectivei/request-demo-cta_t7z1m7.png",
       asset_transform: "f_auto,q_auto,w_800",
       title: "Request a Demo",
       caption: ""
     },
     [9]: {
-      link: "https://collectivei.com/#forecasting",
+      link: "https://web.archive.org/web/20210917081901/https://collectivei.com/#forecasting",
       video: "v1666808142/portfolio/collectivei/forecast_zuvrwp.mp4",
       asset_transform: "f_auto,q_auto,w_1000",
       title: "Forecasts",
       caption: "",
     },
     [10]: {
-      link: "https://collectivei.com/product/#intelligent-insightstm",
+      link: "https://web.archive.org/web/20210917061307/https://collectivei.com/product/#intelligent-insightstm",
       video: "v1666807916/portfolio/collectivei/insights_ayppcj.mp4",
       asset_transform: "f_auto,q_auto,w_1000",
       title: "Intelligent Insights",
       caption: "",
     },
     [11]: {
-      link: "https://collectivei.com/product#virtual-dealroomstm",
+      link: "https://web.archive.org/web/20210917061307/https://collectivei.com/product/#virtual-dealroomstm",
       video: "https://res.cloudinary.com/keenan-payne/video/upload/v1666808439/portfolio/collectivei/virtual-dealrooms_sbf59j.mp4",
       asset_transform: "f_auto,q_auto,w_1300",
       title: "Virtual DealRooms",
