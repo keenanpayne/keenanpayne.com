0c98047da06694bcb0a64fb0a1155ee0352450c0
Keenan Payne <contact@keenanpayne.com>
2024-08-07 14:50:13 -0600

    Update at.inc case study

--- _data/portfolio/atinc.js
+++ _data/portfolio/atinc.js
@@ -4,7 +4,7 @@ module.exports = {
   year: "2020",
   size: "2",
   industry: "Venture Capital",
-  color: "#badfa4",
+  color: "#242529",
   role: "Web Developer",
   url: "https://www.at.inc/",
   services: [
@@ -17,7 +17,7 @@ module.exports = {
     "Netlify CMS",
     "Lottie"
   ],
-  overview: "at.inc is a venture capital firm that invests in early-stage startups. They needed a new website to showcase their portfolio and provide a platform for their team to share their expertise.",
+  overview: "at.inc is a venture capital firm that invests in early-stage startups. They needed a new website to showcase their portfolio and provide a platform for their team to share their expertise. <br><br> I build the website from the ground up, implementing the design and animations, and integrating Netlify CMS to allow the team to easily manage content.",
   lede: "Helping an early-stage venture capital firm showcase their portfolio and services.",
   cover: "v1666928996/portfolio/atinc/cover_hotv7m.png",
   people: {
@@ -35,13 +35,13 @@ module.exports = {
   assets: {
     [1]: {
       link: "https://www.at.inc/",
-      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1666928701/portfolio/atinc/website_nqulvz.png",
+      image: "https://res.cloudinary.com/keenan-payne/image/upload/v1723055894/portfolio/atinc/at-inc-08-07-2024_yhcklw.png",
       asset_transform: "f_auto,q_auto,w_1500",
       title: "Homepage",
     },
     [2]: {
       link: "https://www.at.inc/",
-      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1666929545/portfolio/atinc/website_mc5o3p.mp4",
+      video: "https://res.cloudinary.com/keenan-payne/video/upload/v1723056408/portfolio/atinc/at-inc-08-07-2024_wj8adj.mp4",
       asset_transform: "f_auto,q_auto,w_1500",
       title: "Homepage",
     }
