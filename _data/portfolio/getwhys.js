933cd8885ec5095331fdef5918d90f99d3c3cd51
Keenan Payne <contact@keenanpayne.com>
2025-03-26 10:06:30 -0600

    Add GetWhys case study

---
+++ _data/portfolio/getwhys.js
@@ -0,0 +1,57 @@
+module.exports = {
+  featured: true,
+  name: "GetWhys",
+  project: "Website Redesign and Rebuild",
+  year: "2024–2025",
+  size: "~7",
+  industry: "SaaS — Artificial Intelligence (AI)",
+  color: "#3B4B8D",
+  role: "Web Developer",
+  services: [
+    "Web Design",
+    "Web Development",
+    "CMS Implementation"
+  ],
+  technologies: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "React Router",
+    "Sanity",
+    "Netlify",
+  ],
+  overview: "I helped the GetWhys founders launch their initial website in Q3, 2024 as they solidified their product offerings and signed their first customers before endeavoring to secure seed funding. <br /><br />Six months later, I helped the small but mighty GetWhys team redesign and rebuild their marketing website after <strong>securing $2.75m in seed funding</strong>. <br /><br />The GetWhys team has been delighted by how well <strong>their new website converts</strong>, and how it portrays their growing brand. ",
+  cover: "v1743003774/portfolio/getwhys/getwhys_f3dw1o.png",
+  caption: "",
+  people: {
+    [1]: {
+      name: "Jaime Hernández",
+      position: "Development Partner",
+      url: "https://jaime.one/"
+    },
+  },
+  assets: {
+    [1]: {
+      link: "https://getwhys.io",
+      image: "v1743004021/portfolio/getwhys/getwhys-homepage_usk5g0.png",
+      asset_transform: "f_auto,q_auto,w_1400",
+      title: "GetWhys Homepage",
+      caption: ""
+    },
+    [2]: {
+      link: "https://getwhys.io/blog/meet-getwhys",
+      image: "v1743004020/portfolio/getwhys/getwhys-blog_qsqkac.png",
+      asset_transform: "f_auto,q_auto,w_1400",
+      title: "GetWhys Blog Post",
+      caption: ""
+    },
+    [3]: {
+      link: "https://app.getwhys.io/auth/login",
+      image: "v1743004018/portfolio/getwhys/getwhys-login_bogikn.png",
+      asset_transform: "f_auto,q_auto,w_1400",
+      title: "GetWhys App Login",
+      caption: ""
+    },
+  },
+}
---
+++ portfolio/getwhys.md
@@ -0,0 +1,24 @@
+---
+permalink: '/portfolio/getwhys/'
+title: 'GetWhys'
+meta: 
+  description: "Redesigning and rebuilding the marketing website for an innovative AI platform."
+  image: "https://res.cloudinary.com/keenan-payne/image/upload/v1743003774/portfolio/getwhys/getwhys_f3dw1o.png"
+data: getwhys
+sections: 
+  - type: portfolioGrid
+    project: getwhys
+    modifier: '-one-col'
+    items: 
+      - 1
+  - type: portfolioGrid
+    project: getwhys
+    modifier: '-one-col'
+    items: 
+      - 2
+  - type: portfolioGrid
+    project: getwhys
+    modifier: '-one-col'
+    items: 
+      - 3
+---
