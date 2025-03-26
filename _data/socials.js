43ad0d84e4fdcf58dd43c9e2a0d5341bf09fafba
Keenan Payne <contact@keenanpayne.com>
2025-03-26 10:05:31 -0600

    Add Threads, Mastodon, and Bluesky socials

--- _data/socials.js
+++ _data/socials.js
@@ -1,22 +1,35 @@
 module.exports = {
   [1]: {
-    name: "Twitter",
-    url: "https://twitter.com/KeenanPayne_",
-  },
-  [2]: {
     name: "LinkedIn",
     url: "https://linkedin.com/in/keenanpayne",
   },
-  [3]: {
+  [2]: {
     name: "Dribbble",
     url: "https://dribbble.com/keenanpayne",
   },
-  [4]: {
-    name: "Behance",
-    url: "https://behance.com/keenanpayne",
-  },
-  [5]: {
+  [3]: {
     name: "CodePen",
     url: "https://codepen.com/keenanpayne",
+  },
+  [4]: {
+    name: "Bluesky",
+    url: "https://bsky.app/profile/keenanpayne.bsky.social"
+  },
+  [5]: {
+    name: "Twitter",
+    url: "https://twitter.com/KeenanPayne_",
+  },
+  [6]: {
+    name: "Mastodon",
+    // Format as <a rel="me" href="https://pkm.social/@Keenan">Mastodon</a>
+    url: "https://pkm.social/@Keenan"
+  },
+  [7]: {
+    name: "Threads",
+    url: "https://www.threads.net/@keenancodes"
+  },
+  [8]: {
+    name: "Behance",
+    url: "https://behance.com/keenanpayne",
   },
 }
