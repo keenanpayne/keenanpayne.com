c54b16be395da17a84ba4c4b9628ed467af43535
Keenan Payne <contact@keenanpayne.com>
2025-04-20 12:19:58 -0600

    Create people data file

---
+++ _data/people.js
@@ -0,0 +1,89 @@
+module.exports = {
+  people: [
+    {
+      name: "Jaime Hernández",
+      position: "Development Partner",
+      url: "https://jaime.one/",
+      projects: [
+        "Neuralink",
+        "Transform",
+        "Sparrow",
+        "Refactor Capital",
+        "Collective [i]",
+        "at.inc/"
+      ]
+    },
+    {
+      name: "Play Studio",
+      position: "Design Partner",
+      url: "https://www.play.studio/",
+      projects: [
+        "Neuralink",
+        "Transform",
+        "Sparrow",
+        "Refactor Capital",
+        "Collective [i]"
+      ]
+    },
+    {
+      name: "Keenan Payne",
+      projects: ["Asana"]
+    },
+    {
+      name: "Philippe Boutros",
+      position: "Co-Founder",
+      url: "https://www.linkedin.com/in/philippeboutros",
+      image:
+        "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,w_150/v1743026378/portfolio/getwhys/philippe_cy0mlq.jpg",
+      projects: ["GetWhys"]
+    },
+    {
+      name: "Elliot Matson",
+      position: "Design Partner",
+      url: "https://www.elliotmatson.com",
+      image:
+        "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,w_150/v1743026684/portfolio/getwhys/elliot_a1cwp1.jpg",
+      projects: ["GetWhys"]
+    },
+    {
+      name: "Nels Anderek",
+      position: "Development Partner",
+      url: "https://www.linkedin.com/in/nelsandereck/",
+      projects: ["Sparrow", "Refactor Capital"]
+    },
+    {
+      name: "Ilija Vujinovic",
+      position: "Design Partner",
+      url: "https://dribbble.com/ilijav",
+      projects: ["Rippling"]
+    },
+    {
+      name: "Rob Schneiderman",
+      position: "Development Partner",
+      url: "https://www.linkedin.com/in/robert-schneiderman/",
+      projects: ["Rippling"]
+    },
+    {
+      name: "Kelsea Little",
+      position: "Project Manager",
+      url: "https://www.linkedin.com/in/kelsealittle/",
+      image:
+        "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,w_150/v1657998867/people/kelsea-little_s31zkg.jpg",
+      projects: ["GoFundMe"]
+    },
+    {
+      name: "Tony Mingo",
+      position: "Design Partner",
+      url: "https://tonymingo.com/",
+      image:
+        "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,w_150/v1657998867/people/tony-mingo_zfmkfl.jpg",
+      projects: ["GoFundMe"]
+    },
+    {
+      name: "Paladar Studio",
+      position: "Design Partner",
+      url: "https://www.paladarstudio.com/work/at-inc",
+      projects: ["at.inc/"]
+    }
+  ]
+};
