1ff7247d6fc416891c480782a390ccff809ee19b
Keenan Payne <contact@keenanpayne.com>
2025-04-20 13:03:02 -0600

    Add a unique ID to each testimonial

--- _data/testimonials.js
+++ _data/testimonials.js
@@ -1,66 +1,77 @@
 module.exports = {
   testimonials: [
     {
+      id: 1,
       project: "GetWhys",
       person_id: 4,
       content:
         "Keenan is a wizard and our launch wouldn't have been the same without him. He put together a beautiful site for us, on relatively short notice, at a great rate. He's really easy to communicate with and brings an experienced point-of-view to the table, which we highly valued."
     },
     {
+      id: 2,
       project: "GetWhys",
       person_id: 4,
       content:
         "In the craziness of our launch, Keenan was exactly what we needed—cool-headed, efficient, and dependable. We're planning to keep working with him for as long as he'll have us!"
     },
     {
+      id: 3,
       project: "GetWhys",
       person_id: 5,
       content:
         "I was thrilled when I saw what Keenan had put together based on my brand. He utilized the system I had created and made his own creative decisions about how to best implement it across a variety of pages and UI use cases."
     },
     {
+      id: 4,
       project: "GetWhys",
       person_id: 5,
       content:
         "When I had to work with him closer as the company launch approached, he was awesome to collaborate with, super laid-back, fast and efficient. Would totally recommend him to anyone in need of a developer with a great creative eye."
     },
     {
+      id: 5,
       project: "GoFundMe",
       person_id: 9,
       content:
         "Keenan came well recommended, and after speaking with him I was impressed by his positive attitude and willingness to work into our team workflow."
     },
     {
+      id: 6,
       project: "GoFundMe",
       person_id: 9,
       content:
         "Working with Keenan, we launched the GoFundMe Heroes Hub, a beautiful home for our editorial brand content. It's been very well received and our team loves using it."
     },
     {
+      id: 7,
       project: "GoFundMe",
       person_id: 9,
       content:
         "Keenan was amazing at communication, meeting/exceeding deadlines, and making himself available for fixes & troubleshooting in the immediate stretch after the project had launched."
     },
     {
+      id: 8,
       project: "GoFundMe",
       person_id: 9,
       content:
         "I would recommend Keenan very highly, he was amazing to work with and I hope to be able to work with him again in the future!"
     },
     {
+      id: 9,
       project: "GoFundMe",
       person_id: 10,
       content:
         "Keenan makes a great development partner. As a designer, I appreciate his magical ability to talk about projects in common terms and teach us a few things along the way."
     },
     {
+      id: 10,
       project: "GoFundMe",
       person_id: 10,
       content:
         "I would recommend Keenan to anyone. Aside from his mastery of web development, he's patient, sharp, and empathetic."
     },
     {
+      id: 11,
       project: "GoFundMe",
       person_id: 10,
       content:
