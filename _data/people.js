dfc95d4b046d1773a50bb9a691bd4a3e09a75f1b
Keenan Payne <contact@keenanpayne.com>
2025-04-21 17:40:24 -0600

    Add all people from LinkedIn recommendations

--- _data/people.js
+++ _data/people.js
@@ -90,6 +90,153 @@ module.exports = {
       image:
         "https://res.cloudinary.com/keenan-payne/image/upload/f_auto,w_150/v1657998867/people/tony-mingo_zfmkfl.jpg",
       projects: ["GoFundMe"]
+    },
+    {
+      id: 11,
+      name: "Kelsey Aroian",
+      position: "Brand & Creative, formerly at Front, Asana",
+      url: "https://www.linkedin.com/in/kelseyaroian",
+      projects: ["Asana / Studio Projects"]
+    },
+    {
+      id: 12,
+      name: "Sean Anthony Golliher",
+      position: "President at Metric PPC",
+      url: "https://www.linkedin.com/in/seangolliher",
+      projects: ["Various Client Projects"]
+    },
+    {
+      id: 13,
+      name: "Matt Buckley",
+      position: "Sr Manager, Product Reliability Engineering",
+      url: "https://www.linkedin.com/in/mattebuckley",
+      projects: ["Recording Business Website", "Default Prime"]
+    },
+    {
+      id: 14,
+      name: "Marc Ballinger",
+      position: "Production Management & Forecasting Expert",
+      url: "https://www.linkedin.com/in/marctballinger",
+      projects: ["Mockup Design"]
+    },
+    {
+      id: 15,
+      name: "Austin Slominski",
+      position: "Creative Technologist",
+      url: "https://www.linkedin.com/in/austin-slominski-5733a570",
+      projects: ["Spectral Fusion Designs"]
+    },
+    {
+      id: 16,
+      name: "Kyle Pucko",
+      position: "Entrepreneur, Digital Marketer",
+      url: "https://www.linkedin.com/in/kylepucko",
+      projects: ["Startup Beta Page"]
+    },
+    {
+      id: 17,
+      name: "Jeremy Stein",
+      position: "CEO and Co-Founder @ SportsGrid",
+      url: "https://www.linkedin.com/in/jsstein",
+      projects: ["HAML Template Conversion"]
+    },
+    {
+      id: 18,
+      name: "Tim Peter",
+      position: "Digital Strategy Expert, Tim Peter & Associates",
+      url: "https://www.linkedin.com/in/timpeter",
+      projects: ["Website Redesign"]
+    },
+    {
+      id: 19,
+      name: "James Wasem",
+      position: "Project Manager, Author, Pilot",
+      url: "https://www.linkedin.com/in/james-wasem-49442319",
+      projects: ["Design Collaboration"]
+    },
+    {
+      id: 20,
+      name: "Daniel Mahoney",
+      position: "Broker Associate - Fay Ranches",
+      url: "https://www.linkedin.com/in/daniel-mahoney-8975272b",
+      projects: ["Site Overhaul"]
+    },
+    {
+      id: 21,
+      name: "Amer Rehman",
+      position: "Advisor, Entrepreneur, Investor",
+      url: "https://www.linkedin.com/in/amer-rehman-28aaa",
+      projects: ["Business Website"]
+    },
+    {
+      id: 22,
+      name: "Brian McMahon",
+      position: "Founder @ SparkCures",
+      url: "https://www.linkedin.com/in/brianmcm",
+      projects: ["Feature-Rich Web Design"]
+    },
+    {
+      id: 23,
+      name: "Glenn Nano",
+      position: "Product Leader, ex-Netflix / LA Times",
+      url: "https://www.linkedin.com/in/glennnano",
+      projects: ["AnswerQi Webapp"]
+    },
+    {
+      id: 24,
+      name: "Jamie Forrest",
+      position: "CTO at Coursemojo",
+      url: "https://www.linkedin.com/in/jamieforrest",
+      projects: ["AnswerQi Redesign"]
+    },
+    {
+      id: 25,
+      name: "Alexander Carlson",
+      position: "Vendor Manager at etrailer.com",
+      url: "https://www.linkedin.com/in/alexanderwcarlson",
+      projects: ["Team Leadership"]
+    },
+    {
+      id: 26,
+      name: "Ali Gray",
+      position: "Web Designer at Portland Community College",
+      url: "https://www.linkedin.com/in/ali-gray",
+      projects: ["Team Projects"]
+    },
+    {
+      id: 27,
+      name: "Craig Mcninch",
+      position: "Manager, Collaboration Tools & Services",
+      url: "https://www.linkedin.com/in/craig-mcninch-613a0045",
+      projects: ["Spectral Fusion Designs"]
+    },
+    {
+      id: 28,
+      name: "Nicole Skorka",
+      position: "Commercial Property Manager, Real Estate Broker",
+      url: "https://www.linkedin.com/in/nicoleskorka",
+      projects: ["WordPress Projects"]
+    },
+    {
+      id: 29,
+      name: "Andrew Hartnett",
+      position: "VP of Engineering at Inside Real Estate",
+      url: "https://www.linkedin.com/in/andrewhartnett",
+      projects: ["Front-End Development"]
+    },
+    {
+      id: 30,
+      name: "Brad Tramel",
+      position: "Web Developer at Minnesota Timberwolves and Lynx",
+      url: "https://www.linkedin.com/in/bradtramel",
+      projects: ["Default Prime"]
+    },
+    {
+      id: 31,
+      name: "Lucas Hahn",
+      position: "Manager at PwC, License Management Compliance",
+      url: "https://www.linkedin.com/in/hahnlucas",
+      projects: ["Default Prime"]
     }
   ]
 };
