4dc36d1f0b70fc24dbae8348d6f245c9c78f851d
Keenan Payne <contact@keenanpayne.com>
2025-04-21 18:08:29 -0600

    Append concatenated testimonials

--- _data/testimonials.js
+++ _data/testimonials.js
@@ -244,6 +244,34 @@ module.exports = {
       person_id: 31,
       content:
         "Keenan brought me onto Default Prime and was excellent throughout the process. He created a great team atmosphere and was always available to help."
+    },
+    {
+      id: 36,
+      project: "GetWhys",
+      person_id: 4,
+      content:
+        "Keenan is a wizard and <strong>our launch wouldn't have been the same without him</strong>. He put together a beautiful site for us, on relatively short notice, at a great rate. He's really easy to communicate with and brings an experienced point-of-view to the table, which we highly valued. In the craziness of our launch, Keenan was exactly what we needed—<strong>cool-headed, efficient, and dependable</strong>. We're planning to keep working with him for as long as he'll have us!"
+    },
+    {
+      id: 37,
+      project: "GetWhys",
+      person_id: 5,
+      content:
+        "I was thrilled when I saw what Keenan had put together based on my brand. He utilized the system I had created and made his own creative decisions about how to best implement it across a variety of pages and UI use cases. When I had to work with him closer as the company launch approached, he was awesome to collaborate with, super laid-back, fast and efficient. Would totally recommend him to anyone in need of a developer with a great creative eye. I worked with Keenan on the launch of a website for a new tech startup company. I created the logo and brand identity for GetWhys, and Keenan was given the style guide I made in order to apply it to the website he was developing. He utilized the system I had created and made his own creative decisions. He was awesome to collaborate with, super laid-back, fast and efficient. Would totally recommend him to anyone in need of a developer with a great creative eye."
+    },
+    {
+      id: 38,
+      project: "GoFundMe",
+      person_id: 9,
+      content:
+        "Keenan came well recommended, and after speaking with him I was impressed by his positive attitude and willingness to work into our team workflow. Working with Keenan, we launched the GoFundMe Heroes Hub, a beautiful home for our editorial brand content. It's been very well received and our team <strong>loves using it</strong>. Keenan was amazing at communication, meeting/exceeding deadlines, and making himself available for fixes & troubleshooting in the immediate stretch after the project had launched. I would recommend Keenan very highly, he was amazing to work with and I hope to be able to work with him again in the future!"
+    },
+    {
+      id: 39,
+      project: "GoFundMe",
+      person_id: 10,
+      content:
+        "Keenan makes a great development partner. As a designer, I appreciate his magical ability to talk about projects in common terms and teach us a few things along the way. I would recommend Keenan to anyone. Aside from his mastery of web development, he's patient, sharp, and empathetic. Keenan Payne is like a well-designed website. Technical and complex on the inside, with an intuitive, easy-to-use UI on the outside."
     }
   ]
 };
